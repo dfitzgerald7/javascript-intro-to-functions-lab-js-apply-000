8c363433c1b862bafb309b45ed41b3b67425b4be
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-08-16 17:20:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string){
   console.log(whisper(string))
 }
 
-function sayHitoGrandma(string){
+function sayHiToGrandma(string){
   if (string === string.toLowerCase){
     console.log("I can't hear you")
   }
